102e61b9544959c0d6f100b1531bc6252307c3c9
asnganje <abd.nganje@gmail.com>
2023-11-29 16:45:22 +0300

    Added navbar component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 
 import store from './Redux/store';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,4 +19,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
